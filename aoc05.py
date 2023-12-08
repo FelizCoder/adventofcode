b92ea2f8711d628f493d6fab550104d687fd43b0
elFeliz <felix.kunze@t-online.de>
2023-12-08 17:14:58 +0100

    05 a

---
+++ aoc05.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import pandas as pd
+import re
+
+def aoc05(filepath:str) -> int:
+    seed = None
+    with open(filepath) as f:
+        input = f.read().split(":")
+        
+    # seeds of interest are in the Line after the first ":"
+    # last elemts are declaration of next map
+    
+    seeds = np.int64(input[1].split()[:-2])
+    # All seeds from lowest to highest
+    
+    almanach = pd.DataFrame(seeds, columns=["seeds"])
+    
+    dependencies = ["soil", "fertilizer", "water", "light", "temperature", "humidity", "location"]
+    
+    for i,dep in enumerate(dependencies):
+        # each range is defined in a line after the ":"
+        ranges = input[2+i].splitlines()
+        # whitespace inbetween entries of a range
+        ranges = np.int64([r.split() for r in ranges if re.sub(r'[^\d\s]','', r).strip()])
+        # Perform Mapping
+        ranges
+        almanach[dep] = almanach.iloc[:,-1].apply(lambda x: almanach_mapping(x,ranges))
+    
+    # retun Seed with minimal location
+    return almanach.location.min()
+        
+
+def almanach_mapping(source: np.int_, mapping: np.int_) -> int:
+    """Maps from the Source column entry to Destination
+
+    Args:
+        source (np.int_): Singe entry from source Column
+        mapping (np.int_): Coresponding Mappings
+
+    Returns:
+        int: _description_
+    """    # split mapping into variables
+    map_dest = mapping[:,0]
+    map_source = mapping[:,1]
+    map_length = mapping[:,2]
+
+    # Check where source is within some range
+    idx = np.argwhere(np.logical_and(map_source <= source, source < map_source + map_length))
+    
+    if idx.size > 0:
+        output = map_dest[idx] + source - map_source[idx]
+        return output.flatten()
+    return source
+if __name__ == '__main__':
+    solution = aoc05("05/input.txt")
+    print("The solution is:", solution)
--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from aoc01 import aoc01
 from aoc02 import aoc02
 from aoc03 import aoc03
 from aoc04 import aoc04
+from aoc05 import aoc05
 
 class TestAoc(unittest.TestCase):
     def run_test(self, day: int, expected: int, test_func: Callable[[str], int]):
@@ -29,10 +30,13 @@ class TestAoc(unittest.TestCase):
         self.run_test(2,2286,aoc02)
         
     def test_aoc03(self):
-        self.run_test(3,4361, aoc03)
+        self.run_test(3,467835, aoc03)
         
     def test_aoc04(self):
         self.run_test(4,30,aoc04)
+        
+    def test_aoc05(self):
+        self.run_test(5,35,aoc05)
 
 if __name__ == '__main__':
     unittest.main()
